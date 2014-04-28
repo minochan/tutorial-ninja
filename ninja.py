cad77158ae22c6710cd117ace7dcf445f411bfe5
kminoura <minoruakei@team-lab.com>
2014-04-28 19:06:53 +0900

    fix ninji to ninja

--- ninja.py
+++ ninja.py
@@ -1,6 +1,6 @@
 def ninja(speed, power):
     value = speed * power
     if value > 50:
-        print "This ninjs is strong"
+        print "This ninja is strong"
     else:
         print "you can't trust this ninja"
